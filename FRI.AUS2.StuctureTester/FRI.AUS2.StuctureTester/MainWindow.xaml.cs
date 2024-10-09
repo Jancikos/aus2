a7d00a90c0deb5e3da7c43066ee31f5d1c098cf6
Ján Kostor <ko.janci@gmail.com>
2024-10-09 16:05:53 +0200

    structure tester project init

---
+++ FRI.AUS2.StuctureTester/FRI.AUS2.StuctureTester/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FRI.AUS2.StuctureTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {   
+            InitializeComponent();
+        }
+
+        private void _mnitem_Test_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test!", Title);
+        }
+
+        private void _mnitem_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
