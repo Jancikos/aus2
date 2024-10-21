2cae40398b60da979bd95af57465d2152c0344b4
Ján Kostor <ko.janci@gmail.com>
2024-10-21 08:56:44 +0200

    SP1 gui init

---
+++ FRI.AUS2.SP1/FRI.AUS2.SP1.GUI/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FRI.AUS2.SP1.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region UI Event Handlers
+        private void _mnitem_Test_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test", Title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void _mnitem_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+    #endregion
+}
