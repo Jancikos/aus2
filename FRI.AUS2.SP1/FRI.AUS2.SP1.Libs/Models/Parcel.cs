055057edb08e6bfd18a8f9d877e431bfa076d8c0
Ján Kostor <ko.janci@gmail.com>
2024-10-21 09:01:12 +0200

    SP1 libs init

---
+++ FRI.AUS2.SP1/FRI.AUS2.SP1.Libs/Models/Parcel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRI.AUS2.SP1.Libs.Models
+{
+    internal class Parcel
+    {
+        public double X {  get; set; }
+        public double Y { get; set; }
+    }
+}
