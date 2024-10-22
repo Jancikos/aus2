666a87b83434cb9c638fb0075b8e0022bc670817
Ján Kostor <ko.janci@gmail.com>
2024-10-22 14:20:58 +0200

    kd tree: docs

--- FRI.AUS2.Libs/FRI.AUS2.Libs/Structures/Trees/Interfaces/IKdTreeData.cs
+++ FRI.AUS2.Libs/FRI.AUS2.Libs/Structures/Trees/Interfaces/IKdTreeData.cs
@@ -2,8 +2,18 @@
 {
     public interface IKdTreeData
     {
+        /// <summary>
+        /// -1 - this is smaller
+        ///  0 - this is equal
+        /// +1 - this is bigger 
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public int Compare(int level, IKdTreeData other);
         public int GetDiminesionsCount();
+
+        // POZOR: typ double obmedzuje, ze kluce dimenzii nemozu byt iny typ ako double
         public double GetDiminesionValue(int dim);
     }
 }
