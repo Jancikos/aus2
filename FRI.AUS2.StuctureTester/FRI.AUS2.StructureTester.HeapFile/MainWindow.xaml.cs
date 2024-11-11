de267492f8c736e05df088d36462c622afcb16a8
Ján Kostor <ko.janci@gmail.com>
2024-11-11 15:36:41 +0100

    FRI.AUS2.StructureTester.HeapFile project init

---
+++ FRI.AUS2.StuctureTester/FRI.AUS2.StructureTester.HeapFile/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FRI.AUS2.Libs.Structures.Files;
+
+namespace FRI.AUS2.StructureTester.HeapFileTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HeapFile _strucute;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _strucute = new HeapFile();
+        }
+
+        private void _mnitem_FileClear_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear the file
+            MessageBox.Show("Clearing the file", Title);
+        }
+
+        private void _mnitem_Close_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the application
+            Close();
+        }
+    }
+}
