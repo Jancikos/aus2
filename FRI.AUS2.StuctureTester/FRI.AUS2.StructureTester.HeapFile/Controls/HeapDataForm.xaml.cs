de9c1e8e4178f46c4799f83c987e6f03dd577940
Ján Kostor <ko.janci@gmail.com>
2024-11-11 20:47:53 +0100

    tester: heap data form

---
+++ FRI.AUS2.StuctureTester/FRI.AUS2.StructureTester.HeapFile/Controls/HeapDataForm.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FRI.AUS2.StructureTester.HeapFileTester.Models;
+
+namespace FRI.AUS2.StructureTester.HeapFileTester.Controls
+{
+    /// <summary>
+    /// Interaction logic for HeapData.xaml
+    /// </summary>
+    public partial class HeapDataForm : UserControl
+    {
+        public HeapDataForm()
+        {
+            InitializeComponent();
+        }
+        public int Id
+        {
+            get { return int.Parse(_txtb_Id.Text); }
+            set { _txtb_Id.Text = value.ToString(); }
+        }
+
+        public string Firstname
+        {
+            get { return _txtb_Firstname.Text; }
+            set { _txtb_Firstname.Text = value; }
+        }
+
+        public string Lastname
+        {
+            get { return _txtb_Lastname.Text; }
+            set { _txtb_Lastname.Text = value; }
+        }
+
+        public HeapDataForm HeapData
+        {
+            get
+            {
+                return new HeapDataForm
+                {
+                    Id = Id,
+                    Firstname = Firstname,
+                    Lastname = Lastname
+                };
+            }
+            set
+            {
+                Id = value.Id;
+                Firstname = value.Firstname;
+                Lastname = value.Lastname;
+            }
+        }
+
+        private void _txtb_Generate_Radnom_Int(object sender, MouseButtonEventArgs e)
+        {
+            var random = new Random();
+            var txtb = (TextBox)sender;
+
+            if (txtb.Text == "")
+            {
+                txtb.Text = random.Next(0, 100).ToString();
+                return;
+            }
+
+            if (int.TryParse(txtb.Text, out int result))
+            {
+                txtb.Text = (result + 1).ToString();
+                return;
+            }
+        }
+    }
+}
---
+++ FRI.AUS2.StuctureTester/FRI.AUS2.StructureTester.HeapFile/Models/HeapData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRI.AUS2.StructureTester.HeapFileTester.Models
+{
+    class HeapData
+    {
+        private string _firstname = "";
+        public string Firstname
+        {
+            get => _firstname;
+            set
+            {
+                if (value.Length > 15)
+                {
+                    throw new ArgumentException("Firstname is max 15 characters long");
+                }
+                _firstname = value;
+            }
+        }
+
+        private string _lastname = "";
+        public string Lastname
+        {
+            get => _lastname;
+            set
+            {
+                if (value.Length > 20)
+                {
+                    throw new ArgumentException("Lastname is max 20 characters long");
+                }
+                _lastname = value;
+            }
+        }
+
+        public int Id;
+
+    }
+}
