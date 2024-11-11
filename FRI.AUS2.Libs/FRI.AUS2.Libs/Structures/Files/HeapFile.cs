3a7960cc6a03388466c34632cfb53dbf053171f3
Ján Kostor <ko.janci@gmail.com>
2024-11-11 15:22:55 +0100

    heap f: files init

---
+++ FRI.AUS2.Libs/FRI.AUS2.Libs/Structures/Files/HeapFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRI.AUS2.Libs.Structures.Files
+{
+    public class HeapFile
+    {
+
+    }
+
+    public class HeapFileBlock
+    {
+        
+    }
+}
