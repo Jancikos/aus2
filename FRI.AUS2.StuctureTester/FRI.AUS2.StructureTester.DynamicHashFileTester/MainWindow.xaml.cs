528caa6c156dea86c641618c9e3e56ee5f9c69b7
Ján Kostor <ko.janci@gmail.com>
2024-11-19 13:26:04 +0100

    tester dyn hash file: project init

---
+++ FRI.AUS2.StuctureTester/FRI.AUS2.StructureTester.DynamicHashFileTester/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FRI.AUS2.Libs.Structures.Files;
+
+namespace FRI.AUS2.StructureTester.DynamicHashFileTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // private DynamicHashFile _structure = new DynamicHashFile();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region UI Events
+
+        private void _mnitem_FileClear_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear the file
+            // _structure.Clear();
+            
+            MessageBox.Show("Structure cleared!", Title);
+            // _rerenderAllStats();
+        }
+
+        private void _mnitem_Close_Click(object sender, RoutedEventArgs e)
+        {
+            // Close the application
+            Close();
+        }
+    }
+    #endregion
+}
